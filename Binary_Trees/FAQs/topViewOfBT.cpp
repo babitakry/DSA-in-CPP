038141da7e9d89798bfbde8117a07165158d3739
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-08-19 10:10:02 +0530

    Add top view algorithm for binary trees

---
+++ Binary_Trees/FAQs/topViewOfBT.cpp
@@ -0,0 +1,46 @@
+//Top View of BT
+
+#include<iostream>
+#include<map>
+using namespace std;    
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+};
+
+class Solution{
+    public:
+    vector<int> topView(TreeNode *root){
+        queue<pair<TreeNode*, int>> q;
+        map<int, TreeNode*> mpp;
+
+        q.push({root, 0});
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+
+            TreeNode* node = p.first;
+            int vertical = p.second;
+
+            if(!mpp[vertical])
+              mpp[vertical] = node;
+
+            if(node->left){
+                q.push({node->left, vertical-1});
+            }
+            if(node->right){
+                q.push({node->right, vertical+1});
+            }
+        } 
+
+        vector<int> ans;
+        for(auto it: mpp){
+            TreeNode* node = it.second;
+            ans.push_back(node->data);
+        }
+        return ans;
+    }
+};
