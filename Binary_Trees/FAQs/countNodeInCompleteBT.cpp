9975a7c7d8216fa6c9a1c93e0f1515c54f2f4f3a
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-08-22 14:07:16 +0530

    Add algorithm to count the number of nodes in a complete binary tree

---
+++ Binary_Trees/FAQs/countNodeInCompleteBT.cpp
@@ -0,0 +1,49 @@
+//222. Count Complete Tree Nodes
+
+#include<iostream>
+#include<map>
+#include<set>
+using namespace std;    
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+};
+
+class Solution {
+    int leftHeight(TreeNode* root) {
+        int height = 0;
+        while(root != NULL){
+            height++;
+            root = root->left;
+        }
+
+        return height;
+    }
+
+    int rightHeight(TreeNode* root) {
+        int height = 0;
+        while(root != NULL){
+            height++;
+            root = root->right;
+        }
+
+        return height;
+    }
+public:
+    int countNodes(TreeNode* root) {
+        if (root == NULL) 
+            return 0;
+
+        int lefSubTreeHeight = leftHeight(root);
+        int rightSubTreeHeight = rightHeight(root);
+
+        if (lefSubTreeHeight == rightSubTreeHeight) {
+            return pow(2, lefSubTreeHeight) - 1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
