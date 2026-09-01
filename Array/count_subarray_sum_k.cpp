171113e608b38d0c41a4e7c3a4f5199837515053
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-09-01 20:05:56 +0530

    Add algorithm to count subarrays with a given sum

---
+++ Array/count_subarray_sum_k.cpp
@@ -0,0 +1,26 @@
+//Count subarrays with given sum
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution{
+public:
+    int subarraySum(vector<int> &nums, int k){
+        int n = nums.size();
+        unordered_map<int,int> mpp;
+
+        mpp[0] = 1; // initially
+        int sum = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            int rem = sum - k;
+            if(mpp.find(rem) != mpp.end()){
+                cnt += mpp[rem];
+            }
+            mpp[sum]++ ;
+        }
+        return cnt;
+    }
+};
