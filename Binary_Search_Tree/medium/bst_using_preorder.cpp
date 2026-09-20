ef818d48b9899cf749705a309171286aa02b7168
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-09-20 09:04:27 +0530

    feat: implement BST construction from preorder and inorder traversal

---
+++ Binary_Search_Tree/medium/bst_using_preorder.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std; 
+
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+};
+
+// Better Approach : Using Preorder and Inorder Traversal
+class Solution {
+    TreeNode* bstFromPreorderInorder(vector<int>& preorder, vector<int> &inorder){
+        if(preorder.empty() || inorder.empty()){
+            return NULL;
+        }
+
+        int rootVal = preorder[0];
+
+        int index = -1;
+        for(int i = 0; i < inorder.size(); i++){
+            if(rootVal == inorder[i]){
+                index = i;
+                break;
+            }
+        }
+
+        TreeNode* root = new TreeNode(rootVal);
+
+        vector<int> leftInorder (inorder.begin(), inorder.begin() + index);
+        vector<int> rightInorder (inorder.begin() + index + 1, inorder.end());
+
+        vector<int> leftPreorder (preorder.begin() + 1,  preorder.begin() + 1 + index);
+        vector<int> rightPreorder (preorder.begin() + 1 + index, preorder.end());
+
+        root->left = bstFromPreorderInorder(leftPreorder, leftInorder);
+        root->right = bstFromPreorderInorder(rightPreorder, rightInorder);
+
+        return root;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        vector<int> inorder = preorder;
+        sort(inorder.begin(), inorder.end());
+
+        return bstFromPreorderInorder(preorder, inorder);
+    }
+};
